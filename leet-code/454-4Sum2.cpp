81798fb6f485a5019bdb8338457abec7c1d512c7
Karthik M <karthik.murugesan@outlook.in>
2021-02-05 21:04:19 +0530

    Update 454-4Sum2.cpp

--- leet-code/454-4Sum2.cpp
+++ leet-code/454-4Sum2.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
-        
-        int N = A.size();
-            
-        unordered_map<int, int> x, y;
-        
-        x = sumList(A, B);
-        y = sumList(C, D);
+                    
+        unordered_map<int, int> x;
+        int ans = 0;
 
-        int ans = 0;    
-        for(auto i: x) {
-            if(y[-i.first] != 0) {
-                ans += i.second * y[-i.first];
+        for(auto a : A) {
+            for(auto b : B) {
+                if(x[a + b] == 0) x[a + b] = 1;
+                else x[a + b]++;
+            }
+        }
+        
+        for(auto c : C) {
+            for(auto d : D) {
+                if(x[-c-d] != 0) ans += x[-c-d];    
             }
         }
         
         return ans;
-    }
-    
-    unordered_map<int, int> sumList(vector<int>& p, vector<int>& q) {
-        
-        unordered_map<int, int> temp;
-        int key;
-        
-        for(int i = 0; i < p.size(); i++) {
-            for(int j = 0; j < q.size(); j++) {
-                key = p[i] + q[j];
-                if(temp.find(key) == temp.end()) {
-                    temp[key] = 1;
-                }
-                else {
-                    temp[key]++;
-                }
-            }
-        }
-        
-        return temp;
     }
 };
