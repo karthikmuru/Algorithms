9b337fe623a4f17bffdbd8a896f6baa3139e9ca2
Karthik M <karthik.murugesan@outlook.in>
2021-02-05 23:28:57 +0530

    Create 389-FindTheDifference.cpp

---
+++ leet-code/389-FindTheDifference.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        const int MAXCHAR = 26;
+        
+        int charCount[MAXCHAR] = {0};
+        
+        for(auto i : s)
+            charCount[i - 'a']++;
+        
+        for(auto i : t)
+            charCount[i - 'a']++;
+ 
+        for(int i = 0; i < MAXCHAR; i++) {
+            if(charCount[i] % 2 != 0) return (char)(i + 'a');
+        }
+        
+        return ' ';
+    }
+};
