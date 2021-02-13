34ade3156e4681f1d61d4e64cfac8a745ac83159
Karthik M <karthik.murugesan@outlook.in>
2021-02-13 16:46:59 +0530

    Create 991-BrokenCalculator.cpp

---
+++ leet-code/991-BrokenCalculator.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        
+        int ans = 0;
+        
+        while(X!=Y) { 
+            
+            if (Y <= X) return ((X - Y) + ans);
+            if(Y % 2 == 0) Y /= 2;
+            else Y ++;
+            
+            ans ++;
+        }
+        return ans;
+    }
+};
