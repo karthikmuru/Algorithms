763574730f3b79a72ce233ff24d19ce6b7bddd2d
Karthik M <karthik.murugesan@outlook.in>
2021-02-13 17:24:07 +0530

    Create 260-SingleNumberIII.cpp

---
+++ leet-code/260-SingleNumberIII.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        
+        vector<int> ans;
+        int i = 0;
+
+        sort(nums.begin(), nums.end());
+        
+        while(i < nums.size() - 1) {
+                
+            if(nums[i] == nums[i + 1]) i += 1;
+            else ans.push_back(nums[i]);
+                
+            i += 1;
+        }
+        
+        if(i == nums.size() - 1) ans.push_back(nums.back());
+        
+        return ans;
+    }
+};
